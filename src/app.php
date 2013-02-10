d82b0a8bc36110c2717ad426808150b3da697450
Nik Cubrilovic <nikcub@gmail.com>
2013-02-10 11:06:40 +1100

    template and other variables

--- src/app.php
+++ src/app.php
@@ -3,41 +3,101 @@
 namespace Webwall;
 // use Symfony\Component\HttpKernel\;
 
-use Symfony\Component\HttpKernel;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\HttpKernel;
+use Symfony\Component\HttpKernel\EventListener\ExceptionListener;
+use Symfony\Component\HttpKernel\HttpCache\HttpCache;
+use Symfony\Component\HttpKernel\HttpCache\Store;
+
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
 use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-
-
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RequestContext;
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+use Truss;
 use Truss\Application;
 use Truss\RequestHandler;
+use Truss\ResponseEvent;
+use Truss\ControllerResolver;
 
+$logger = new Logger('webwall');
+$log_handler = new StreamHandler(WEBWALL_ROOT . '/var/log/app_log', Logger::DEBUG);
+$logger->pushHandler($log_handler);
+$logger->addDebug('Started');
 
 $request = Request::createFromGlobals();
+
 $routes = new RouteCollection();
- 
-// $routes->add('hello', new Route('/hello/{name}', array('name' => 'World')));
-// $routes->add('bye', new Route('/bye'));
-$routes->add('api', new Route('/api/{method}', array('method' => 'Index', '_controller' => 'Webwall\Controllers\ApiController::get')));
+// $routes->add('api', new Route('/api/{method}', array('method' => 'Index', '_controller' => 'Webwall\Controllers\ApiController::get')));
+// $routes->add('api', new Route('/test/{method}', array('method' => 'Index', '_controller' => 'Webwall\Controllers\ApiController::test')));
+// $routes->add('api', new Route('/post/{id}', array('method' => 'Index', '_controller' => 'Webwall\Controllers\PostController::get')));
+$routes->add('posts', new Route('/posts/{stub}', array('_controller' => 'Webwall\Controllers\TestController::_method')));
+
+$routes->add('test', new Route('/test', array('_controller' => 'Webwall\Controllers\IndexController::get'), array('_method' => 'GET')));
+
+$routes->add('d0', new Route('/{_object}/{_method}/{_params}', array('_object' => 'index', '_method' => 'get', '_params' => ''), array('_params' => '.*', '_method' => 'get')));
+// $routes->add('d1', new Route('/{_object}/{_method}'));
+// $routes->add('d2', new Route('/{_object}'));
+// $routes->add('d3', new Route('/'));
 
 
 
 $context = new RequestContext();
 $context->fromRequest($request);
+
 $matcher = new UrlMatcher($routes, $context);
 
-// $dispatcher = new EventDispatcher();
-// $dispatcher->addSubscriber(new RouteListener($matcher));
+$resolver = new Truss\DynamicControllerResolver("Webwall\Controllers");
 
-$resolver = new ControllerResolver();
 
-$app = new Application($matcher, $resolver);
-$response = $app->dispatch($request);
+$dispatcher = new EventDispatcher();
+$dispatcher->addSubscriber(new ExceptionListener('Webwall\\Controllers\\ErrorController::exceptionAction'));
+$dispatcher->addSubscriber(new Truss\Listeners\ContentLengthListener());
+$dispatcher->addSubscriber(new Truss\Listeners\StringResponseListener());
+$dispatcher->addSubscriber(new Truss\Listeners\ArrayResponseListener());
+// $dispatcher->addSubscriber(new Truss\Listeners\DefaultRouteListener());
+$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher));
+
+
+
+class GenericApp extends HttpKernel\HttpKernel {
+
+}
+
+
+
+$cache_dir = WEBWALL_WEBROOT . '/../var/cache';
+if(!is_dir($cache_dir)) {
+	throw new Exception('bad cache dir ' . $cache_dir);
+}
+$store = new Store($cache_dir);
+
+$app = new Application($dispatcher, $resolver);
+// $app = new GenericApp($dispatcher, $resolver);
+// $app = new HttpCache($app, $store);
+// $app->handle($request)->send();
+
+$app->run();
+
+// $app->handle($request)->send();
+
+// $app2 = new GenericApp($dispatcher, $resolver);
+// $app2 = new HttpCache($app2, $store);
+// $app2->handle($request)->send();
+
+
+// $app = new Application($dispatcher, $resolver, $matcher);
+// $app = new HttpCache($app, $store);
+// $app->handle($request)->send();
+
+// $response = $app->dispatch($request);
 
 // $kernel = new HttpKernel($dispatcher, $resolver);
 
@@ -45,4 +105,4 @@ $response = $app->dispatch($request);
 // return 1;
 
 
-$response->send();
+// $response->send();
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,13 +1,44 @@
 <?php
- 
-// framework/autoload.php
- 
-// require_once __DIR__.'/vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
- 
-// use Symfony\Component\ClassLoader\UniversalClassLoader;
- 
-// $loader = new UniversalClassLoader();
-// $loader->register();
 
-require __DIR__ . '/../vendor/autoload.php';
+// Global vars used throughout
+define('TRUSS_VERSION', '0.1.2');
+define('TRUSS_PHP_REQUIRED', '5.3.0');
+define('DS', DIRECTORY_SEPARATOR);
+define('PS', PATH_SEPARATOR);
+define('NL', "\n");
+
+date_default_timezone_set('UTC');
+
+ini_set('date.timezone', 'UTC');
+
+setlocale(LC_ALL, 'en_US.utf-8');
+
+ini_set('display_errors', true);
+
+@ini_set('cgi.fix_pathinfo', 0);
+
+if(version_compare(PHP_VERSION, TRUSS_PHP_REQUIRED) == -1) {
+	exit('Requires PHP v' . TRUSS_PHP_REQUIRED);
+}
+
+if (!isset($_SERVER['DOCUMENT_ROOT'])) {
+  $_SERVER['DOCUMENT_ROOT'] = substr($_SERVER['SCRIPT_FILENAME'], 0, -strlen($_SERVER['SCRIPT_NAME']));
+}
+
+// defined('PATH_LIBRARY') or define('PATH_LIBRARY', realpath(dirname(__file__) . DS . '..' . DS));
+// defined('PATH_PUBLIC') or define('PATH_PUBLIC', (is_dir($_SERVER['DOCUMENT_ROOT'])) ? str_replace('/', DS, $_SERVER['DOCUMENT_ROOT']) : null);
+// define('PATH_TRUSS', dirname(__FILE__));
+// define('PATH_BASE', realpath(PATH_PUBLIC . DS . '..' . DS));
+// defined('PATH_APP') or define('PATH_APP', realpath(PATH_BASE . DS . 'app'));
+
+// ini_set('include_path', PATH_TRUSS . PS . ini_get('include_path'));
+
+define('WEBWALL_THEME', realpath(WEBWALL_ROOT . '/themes'));
+
+define('TRUSS_ENV', 'dev');
+
+
+
+require WEBWALL_ROOT . '/vendor/autoload.php';
+
 require 'app.php';
